4a1ee16d231c812b780b832b1a2a247423d42488
Ara <Ara@DESKTOP-669DI6D>
2016-12-12 18:28:36 +0100

    garaxe

--- src/boletin12/Boletin12.java
+++ src/boletin12/Boletin12.java
@@ -1,12 +1,31 @@
 
 package boletin12;
 
+import javax.swing.JOptionPane;
+
 
 public class Boletin12 {
 
     public static void main(String[] args) {
-      Garaxe obx1= new Garaxe();
-      obx1.aparcar();
+      //while((Garaxe.getNumPlazas()-Garaxe.getNumeroCoches())>0){
+      
+      Garaxe aparca1= new Garaxe("1234AB");
+      aparca1.aparcar(); 
+      aparca1.prezoHora();
+      aparca1.amosarFactura();
+      
+      Garaxe aparca2= new Garaxe("0987VB");
+      aparca2.aparcar();
+      aparca2.prezoHora();
+      aparca2.amosarFactura();
+      
+      /*Garaxe aparca3= new Garaxe("3456GH");
+      aparca3.aparcar();
+      Garaxe aparca4= new Garaxe("6789VB");
+      aparca4.aparcar();
+      Garaxe aparca5= new Garaxe("6780NB");
+      aparca5.aparcar();*/
+     
     }
     
 }
--- src/boletin12/Garaxe.java
+++ src/boletin12/Garaxe.java
@@ -1,25 +1,85 @@
 
 package boletin12;
 
+
+import javax.xml.datatype.Duration;
+import java.time.Instant;
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class Garaxe {
-    private int numeroCoches, matricula;
+    private static int numCoches=0, numPlazas=5;
+    private String matricula;
+    private final float menos3H=1.50f;
+    private final float mas3H=0.20f;
+    private int horas;
+
+    public Garaxe() {
+        numCoches++;
+    }
+
+    public Garaxe(String matricula) {
+        numCoches++;
+        this.matricula=matricula;
+        
+        
+    }
     
+    public static int getNumeroCoches() {
+        return numCoches;
+    }
+
+    public static int getNumPlazas() {
+        return numPlazas;
+    }
+    public String matricula(){
+        Scanner entrada=new Scanner(System.in);
+        return entrada.nextLine();
+        
+    }
     public void aparcar (){
-     int cont;
-     numeroCoches=Integer.parseInt(JOptionPane.showInputDialog("Introduza o número de coches"));
-     
-         if(numeroCoches<=5){
-             System.out.println("Plazas dispoñibles");
-             numeroCoches=numeroCoches+1;
+    
+         if(numCoches<numPlazas&&numCoches>=0){
+             System.out.println("Plazas dispoñibles " + (numPlazas-numCoches));
+         
          }
              else{
-             System.out.println ("Completo");
+             System.out.println ("Completo" + numCoches);
          }
-             
-         
-     }
     }
+    public void prezoHora(){
+        horas=Integer.parseInt(JOptionPane.showInputDialog("Introduce as horas que estivo no parking: "));
+        if(horas<=3){
+            System.out.println("Precio horas: " + (menos3H));
+        }
+        else{
+            System.out.println("Precio:" + ((horas-3)*mas3H+menos3H));
+        }
+        }
+    public void amosarFactura(){
+        System.out.println("Matricula: " + matricula);
+        System.out.println("Tempo: " + horas + " horas.");
+        int cartos=Integer.parseInt(JOptionPane.showInputDialog("Introduce cartos para pagar o parking "));
+        if (horas<3)
+            System.out.println("Cartos devoltos: " + (cartos- menos3H));
+        else 
+            System.out.println("Cartos devoltos: " + (cartos -((horas-3)*mas3H+menos3H) ));
+        
+        
+        
+        
+    }
+        
+        
+        
+        
+    }
+    
+   
+
+
+
+
     
 
